fb9cd9e4e2fc8ef0482829252f4c5bd9ed2c589a
Kyle L <likyle001@gmail.com>
2020-12-31 02:07:17 -0500

    responsive grid layout

---
+++ grid.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    var grid = document.querySelector('.grid');
+    var msnry = new Masonry( grid, {
+    // option,
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    gutter: '.gutter-sizer',
+    horizontalOrder: true,
+    percentPosition: true
+    });
+    
+
+
+    $(".card").hover(function () {
+        //stuff to do on mouse enter
+        console.log("hovering");
+        msnry.layout();
+
+        
+    }, 
+    function () {
+        console.log("unhover");
+        msnry.layout();
+
+        //stuff to do on mouse leave
+    });
+
+
+    textfit();
+    $(window).on('resize', textfit);
+
+    function textfit() {
+        console.log("resizing card text")
+
+        var cardWidth = $(".card").width();
+
+        $(".card-title").css("font-size", cardWidth * (19/(1056 * .3)) + "px");
+        $(".card-subtitle").css("font-size", cardWidth * (11/(1056 * .3)) + "px");
+        $(".explore").css("font-size", cardWidth * (10/(1056 * .3)) + "px");
+        $(".card-content").css("font-size", cardWidth * (15/300) + "px");
+
+    }
+
+});
